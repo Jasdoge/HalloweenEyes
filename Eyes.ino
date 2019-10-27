42adcb7fdabdff298732c4e2b4ef3d4ce2962da8
Jasdoge <jasdoge@gmail.com>
2019-10-27 20:11:05 +0100

    comment

--- Eyes.ino
+++ Eyes.ino
@@ -1,74 +1,59 @@
+// Include the arduino library
 #include <arduino.h>
+// Include the libraries needed to use sleep
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 #include <avr/power.h>
 
-const unsigned int WAKE_TIME = 60000;
-const byte LIGHTS[4] = {PB0, PB1, PB2, PB3};
-const byte PIN_SENSOR = PCINT4;
-unsigned long WOKE = 0;
+const unsigned int WAKE_TIME = 60000;				// How long should it be awake and blinking before shutting down? In milliseconds.
+const byte LIGHTS[4] = {PB0, PB1, PB2, PB3};		// Pins the lights are bound to. See circuit file in assets folder.
+const byte PIN_SENSOR = PCINT4;						// Sensor pin
+unsigned long WOKE = 0;								// millis() reading of woke time
 
-ISR(PCINT0_vect){}
+ISR(PCINT0_vect){}									// Needed to prevent crashing
 
+// Activates sleep mode
 void sleep(){
 
 	GIMSK |= _BV(PCIE);    // turns on pin change interrupts
   	PCMSK |= _BV(PIN_SENSOR);    // turn on interrupt on sensor pin
 
 	// Enters sleep mode
-    //sleep_enable();                          // enables the sleep bit in the mcucr register so sleep is possible
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // replaces above statement
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 	sleep_mode();
 
-  	PCMSK &= ~_BV(PIN_SENSOR);    // interrupt off
-
-}
-
-// Animates the eyes up
-void wakeAnim(){
-
-	byte order[4] = {0xFF,0xFF,0xFF,0xFF};
-	for( byte i=0; i<4; ++i ){
-		const byte slot = millis()%(4-i);
-		byte n = 0;
-		for( byte s=0; s<4; ++s ){
-			if( order[s] == 0xFF ){
-				if( n == slot ){
-					order[s] = LIGHTS[i];
-					break;
-				}
-				++n;
-			}
-		}
-	}
-
-	for( byte i=0; i<4; ++i ){
-		digitalWrite(order[i], HIGH);
-		delay(100+(millis()%1000));
-	}
+  	PCMSK &= ~_BV(PIN_SENSOR);    // interrupt off when waking up
 
 }
 
 void setup(){
 	
-	for( byte i=0; i<4; ++i ){
+	ADCSRA &= ~_BV(ADEN); // disable the ADC to save power
+
+	byte i;
+	// Set pin modes and turn on all lights on boot so you can see that they all work.
+	for( i=0; i<4; ++i ){
 
 		pinMode(LIGHTS[i], OUTPUT);
-		digitalWrite(LIGHTS[i], LOW);
+		digitalWrite(LIGHTS[i], HIGH);
 	
 	}
 
-	ADCSRA &= ~_BV(ADEN); // disable the ADC
-	wakeAnim();
+	// Wait 2 sec before turning them off
+	delay(2000);
+	for( i=0; i<4; ++i )
+		digitalWrite(LIGHTS[i], LOW);
 
 }
 
+
 void loop(){
 
+	// Blinks the eyes for WAKE_TIME milliseconds at random intervals.
+	// This way the eyes will light up somewhat randomly, all eyes might not light up. But I thought it was a neat effect.
 	const unsigned long ms = millis();
 	if( ms-WOKE < WAKE_TIME ){
 
-		
 		const byte pin = LIGHTS[ms%4];
 		digitalWrite(pin, LOW);
 		delay(100);
@@ -76,12 +61,12 @@ void loop(){
 
 		delay((ms%750)+200);
 		return;
+		
 	}
 
-
-	for( byte i=0; i<4; ++i ){
+	// Turn off after that
+	for( byte i=0; i<4; ++i )
 		digitalWrite(LIGHTS[i], LOW);
-	}
 	sleep();
 	WOKE = millis();
 	
